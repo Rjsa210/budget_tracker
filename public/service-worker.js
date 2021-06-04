289fe1c7cd03cef8253d820607bae5e630124c41
Reid Johnson <rjsa210@gmail.com>
2021-06-03 22:12:59 -0700

    depoying to heroku -RJ

--- public/service-worker.js
+++ public/service-worker.js
@@ -45,28 +45,6 @@ self.addEventListener('activate', event => {
   );
 });
 
-
-// self.addEventListener('fetch', (event) => {
-//   console.log('Fetching')
-//   if (event.request.url.startsWith(self.location.origin)) {
-//     event.respondWith(
-//       caches.match(event.request).then((cachedResponse) => {
-//         if (cachedResponse) {
-//           console.log('cached response found')
-//           return cachedResponse;
-//         }
-//         return caches.open(RUNTIME_CACHE).then((cache) => {
-//           return fetch(event.request).then((response) => {
-//             return cache.put(event.request, response.clone()).then(() => {
-//               return response;
-//             });
-//           });
-//         });
-//       })
-//     );
-//   }
-// });
-
 //call fetch event
 self.addEventListener('fetch', (event) => {
   console.log('Fetching')
--- server.js
+++ server.js
@@ -14,7 +14,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/budget", 
+  {
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useNewUrlParser: true,
   useFindAndModify: false
 });
